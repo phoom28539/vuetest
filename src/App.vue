06e89cb6d33b809ec68a7812afff5b7859b78b5c
Phoompat Jirajaran <phoom_10972@hotmail.com>
2020-10-21 20:16:00 +0700

    update file

---
+++ src/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <div id="app" class="small-container">
+    <h1>Employees</h1>
+    <employee-table :employees="employees" />
+  </div>
+</template>
+
+<script>
+import EmployeeTable from '@/components/EmployeeTable'
+export default {
+  name: 'App',
+  components:{
+    EmployeeTable
+  },
+  data() {
+    return {
+      employees: [
+        {
+          id: 1,
+          name: 'phoom jr',
+          email: 'phoom@hotmail.com',
+        },
+        {
+          id: 2,
+          name: 'Te jr',
+          email: 'Ter@hotmail.com',
+        },
+        {
+          id: 3,
+          name: 'plo jr',
+          email: 'plor@hotmail.com',
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+  .small-container {
+    max-width: 680px;
+  }
+</style>
---
+++ src/components/EmployeeTable.vue
@@ -0,0 +1,52 @@
+<template>
+    <div id="employee-table">
+        <table>
+            <thead>
+                <tr>
+                    <th>Employee name</th>
+                    <th>Employee email</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="employee in employees" :key="employee.id">
+                    <td>{{ employee.name }}</td>
+                    <td>{{ employee.email }}</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'employee-table',
+    props: {
+        employees: Array
+    }
+}
+</script>
+
+<style scoped>
+    table {
+    font-family: "Trebuchet MS", Arial, Helvetica, sans-serif;
+    border-collapse: collapse;
+    width: 100%;
+    }
+
+    table td, table th {
+    border: 1px solid #ddd;
+    padding: 8px;
+    }
+
+    table tr:nth-child(even){background-color: #f2f2f2;}
+
+    table tr:hover {background-color: #ddd;}
+
+    table th {
+    padding-top: 12px;
+    padding-bottom: 12px;
+    text-align: left;
+    background-color: #4CAF50;
+    color: white;
+    }
+</style>
